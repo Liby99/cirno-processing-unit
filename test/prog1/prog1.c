f4fe421545fd23cdb04e6c1646f67afa483367e0
Liby Lee <liby99@icloud.com>
2019-04-05 17:45:01 -0700

    Add template and placeholder

---
+++ test/prog1/prog1.c
@@ -0,0 +1,20 @@
+const int MEM_SIZE = 256;
+
+int main() {
+  char * mem = calloc(sizeof(char) * MEM_SIZE);
+  setup(mem);
+  prog2(mem);
+  test(mem);
+}
+
+void setup(char* mem) {
+  // Setup
+}
+
+void prog2(char* mem) {
+  // What i need to write
+}
+
+void test(char* mem) {
+  // What i need to test
+}
