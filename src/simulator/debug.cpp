d31632282315686ac91585b794e1abf865006a3f
Liby Lee <liby99@icloud.com>
2019-04-09 02:57:29 -0700

    Add assembler debugging label generation

--- src/simulator/debug.cpp
+++ src/simulator/debug.cpp
@@ -7,16 +7,22 @@ void print_prompt() {
   printf("(cdb) > ");
 }
 
+void print_unknown_command(const std::string &user_input) {
+  printf("Unknown command \"%s\"\n", user_input.c_str());
+}
+
+void print_entry(const std::string &filename) {
+  printf("Debugging program %s. Type `help` if you want more commands.\n", filename.c_str());
+}
+
 void print_help() {
   printf("These options are available for you...\n");
   printf("  help\n");
   printf("  step\n");
   printf("  run\n");
+  printf("  list\n");
   printf("  break [line_num]\n");
-  printf("  print pc\n");
-  printf("  print regs\n");
-  printf("  print mem\n");
-  printf("  print mem [position]\n");
+  printf("  print [pc | reg | cmp | mem | mem [loc] | mem [start] [end] | breakpoint]\n");
 }
 
 std::vector<std::string> split(std::string str, char delimiter) {
@@ -31,16 +37,118 @@ std::vector<std::string> split(std::string str, char delimiter) {
   return internal;
 }
 
+void print_mem_at(CPU &cpu, int index) {
+  printf("mem[%d]: ", index);
+  print_binary(cpu.memory[index]);
+  printf("\n");
+}
+
+bool should_stop(CPU &cpu, std::vector<int> &breakpoints) {
+  if (cpu.pc >= cpu.instructions.size()) {
+    return true;
+  } else {
+    for (int i = 0; i < breakpoints.size(); i++) {
+      int bp = breakpoints[i];
+      if (cpu.pc == bp) {
+        return true;
+      }
+    }
+    return false;
+  }
+}
+
+void run_cpu(CPU &cpu, std::vector<int> &breakpoints) {
+  while (!should_stop(cpu, breakpoints)) {
+    cpu.step();
+  }
+}
+
 void exec_user_input(const std::string &user_input, std::vector<int> &breakpoints, CPU &cpu) {
   std::vector<std::string> trimmed = split(user_input, ' ');
-  printf("[");
-  for (int i = 0; i < trimmed.size(); i++) {
-    printf("%s", trimmed[i].c_str());
-    if (i < trimmed.size() - 1) {
-      printf(", ");
+  if (trimmed.size() > 0) {
+    if (trimmed[0] == "help") {
+      print_help();
+    } else if (trimmed[0] == "step") {
+      cpu.step();
+    } else if (trimmed[0] == "run") {
+      run_cpu(cpu, breakpoints);
+    } else if (trimmed[0] == "break") {
+      if (trimmed.size() > 1) {
+        try {
+          int index = atoi(trimmed[1].c_str());
+          if (index >= 0 && index < cpu.instructions.size()) {
+            breakpoints.push_back(index);
+            printf("Added breakpoint #%lu at line %d\n", breakpoints.size(), index);
+          } else {
+            printf("index has to be >= 0 and < 256\n");
+          }
+        } catch (...) {
+          printf("Cannot parse index %s\n", trimmed[1].c_str());
+        }
+      } else {
+        printf("Please specify the line number you want to set breakpoint.");
+        printf("Usage: break [line_num]\n");
+      }
+    } else if (trimmed[0] == "list") {
+      int start = std::max(cpu.pc - 5, 0);
+      int end = std::min(cpu.pc + 5, (int) cpu.instructions.size());
+      for (int i = start; i < end; i++) {
+        Instr instr = cpu.instructions[i];
+        printf("%s %d: %d", i == cpu.pc ? "->" : "  ", i, (instr >> 8) & 1);
+        print_binary((byte) instr);
+        printf("\n");
+      }
+    } else if (trimmed[0] == "print") {
+      if (trimmed.size() > 1) {
+        if (trimmed[1] == "pc") {
+          printf("pc: %d\n", cpu.pc);
+        } else if (trimmed[1] == "reg") {
+          cpu.print_regs();
+        } else if (trimmed[1] == "cmp") {
+          printf("cmp: %d\n", cpu.cmp);
+        } else if (trimmed[1] == "mem") {
+          if (trimmed.size() > 2) {
+            try {
+              int index_1 = atoi(trimmed[2].c_str());
+              if (index_1 >= 0 && index_1 < 256) {
+                if (trimmed.size() > 3) {
+                  try {
+                    int index_2 = atoi(trimmed[3].c_str());
+                    if (index_2 >= 0 && index_2 < 256) {
+                      if (index_1 <= index_2) {
+                        for (int i = index_1; i <= index_2; i++) {
+                          print_mem_at(cpu, i);
+                        }
+                      } else {
+                        printf("start has to be less than or equal to end\n");
+                      }
+                    } else {
+                      printf("index has to be >= 0 and < 256\n");
+                    }
+                  } catch (...) {
+                    printf("Cannot parse index %s\n", trimmed[3].c_str());
+                  }
+                } else {
+                  print_mem_at(cpu, index_1);
+                }
+              } else {
+                printf("index has to be >= 0 and < 256\n");
+              }
+            } catch (...) {
+              printf("Cannot parse index %s\n", trimmed[2].c_str());
+            }
+          } else {
+            cpu.print_memory();
+          }
+        }
+      } else {
+        printf("Please specify the thing you want to print.\n");
+        printf("Usage: print [pc|reg|cmp|mem|mem [location]]\n");
+      }
+    } else {
+      print_unknown_command(user_input);
     }
   }
-  printf("]\n");
 }
 
 int main(int argc, char *argv[]) {
@@ -67,7 +175,7 @@ int main(int argc, char *argv[]) {
   // Create the CPU
   CPU cpu(instructions, memory);
 
-  print_help();
+  print_entry(argv[1]);
   print_prompt();
 
   // Run the CPU until Halt
