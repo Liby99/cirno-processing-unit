eb1f3ae8027a35ce276bfa5c09edb364a75c6a6f
dingmei <d3gu@ucsd.edu>
2019-04-09 12:51:36 -0700

    prog1.s v2

--- test/prog1/prog1.s
+++ test/prog1/prog1.s
@@ -14,287 +14,181 @@
 #define addr_temp_lower 208
 #define addr_temp_upper 209
 
-
+prog_start:
 	andi	$0, 0
-
-	mov	$1, addr_i
+	movi	$1, addr_i
 	st	$0, $1
-	mov $1, addr_j
+	movi $1, addr_lower
 	st	$0, $1
-	mov $1, addr_t
+	movi	$1, addr_upper
 	st	$0, $1
-	mov $1, addr_p
+	movi $1, addr_temp_lower
 	st	$0, $1
-	mov $1, addr_p8
-	st	$0, $1
-	mov $1, addr_p4
-	st	$0, $1
-	mov $1, addr_p2
-	st	$0, $1
-	mov $1, addr_p1
-	st	$0, $1
-	mov $1, addr_lower
-	st	$0, $1
-	mov	$1, addr_upper
-	st	$0, $1
-	mov $1, addr_b4to2
-	st	$0, $1
-	mov $1, addr_b1
-	st	$0, $1
-	mov $1, addr_temp_lower
-	st	$0, $1
-	mov $1, addr_temp_upper
+	movi $1, addr_temp_upper
 	st	$0, $1
 
 	andi	$0, 30
-	mov		$1, addr_k
+	movi		$1, addr_k
 	st		$0, $1
 
 while:
-	mov	$1,	addr_i
+	movi	$1,	addr_i
 	ld	$1, $1
 	andi	$0, 30
 	cmp	$0, $1
-	beq	end_while
+	beqil	end_while
 
-	mov	$0, addr_lower
-	ld	$2, $1					// $2 = mem[i]
-	st	$2, $0					// lower = mem[i]????
+	movi	$0, addr_lower
+	ld	$2, $1					// $2(lower) = mem[i]
+	st	$2, $0
 
 	incr	$1
-	mov	$0, addr_upper
-	ld	$2, $1					// $2 = mem[i]
-	st	$2, $0					// upper = mem[i]
+	ld	$3, $1					// $3(upper) = mem[i]
 	incr	$1
+	movi	$0, addr_i
+	st	$1, $0
 
-	shli	$0, 4
-	mov	$2, addr_lower
+	shli	$3, 4
 	shri	$2, 4
-	or	$0, $2					// $0 = (upper << 4) | (lower >> 4)
-	mov	$2, addr_temp_upper
-	st	$0, $2					// temp_upper = $0
+	or	$3, $2					// $3 = (upper << 4) | (lower >> 4)
+	movi	$2, addr_temp_upper
+	st	$3, $2					// $3(temp_upper) = mem[$2]
 
 p8:
-	mov	$2, addr_p
-	st	$0, $2
-	shri	$0, 1
-	mov	$3, addr_t
-	st	$0, $3
-
-	andi $0, 0
-	mov	$1, addr_j
-	st	$0, $1					// j = 0
+	mov	$0, $3					// $0 = p
+	mov	$1, $0
+	shri	$1, 1					// $1 = t
+	clr	$2							// $2(j) = 0
+	movi	$3, 7					// $3 = 7
 
 p8while:
-	mov	$1, addr_j
-	ld	$1, $1
-	andi	$0, 7
-	cmp	$0, $1
-	beq	p4
-
-	mov	$2, addr_p
-	ld	$2, $2					// p
-	mov	$3, addr_t
-	ld	$3, $3					// t
-	xor	$2, $3
-	mov	$0, addr_p
-	st	$2, $0
-	shri	$3, 1
-	mov	$0, addr_t
-	st	$3, $0
-	incr	$1
-	mov	$0, addr_j
-	st	$1, $0
+	xor	$0, $1
+	shri	$1, 1
+	incr	$2
+	cmp	$3, $2
+	beqil p4
+	jmpil	p8while
 
 p4:
-	mov	$0, addr_p
-	ld	$0, $0
-	shli	$0, 7
-	andi	$1, 128
-	and	$0, $1
-	mov	$1, addr_p8
+	shli	$0, 7					// p << 7
+	andi	$0, 128
+	movi	$1, addr_p8
 	st	$0, $1
 
-	mov	$0, addr_temp_upper
+	movi	$0, addr_temp_upper
 	ld	$0, $0
-	andi	$1, 248
-	and	$0, $1					// $0 = (temp_upper & 248)
-	mov	$1, addr_lower
+	andi	$0, 248				// $0 = (temp_upper & 248)
+	movi	$1, addr_lower
 	ld	$1, $1
 	shri	$1, 1
-	andi	$2, 7
-	and	$1, $2					// $1 = ((lower >> 1) & 7)
-	or	$0, $1
-	mov	$1, addr_p
-	st	$0, $1
-	shri	$0, 1
-	mov	$1, addr_t
-	st	$0, $1
-
-	andi $0, 0
-	mov	$1, addr_j
-	st	$0, $1					// j = 0
+	andi	$1, 7					// $1 = ((lower >> 1) & 7)
+	or	$0, $1					// $0 = p
+	mov	$1, $0
+	shri	$1, 1					// $1 = t
+	clr	$2							// j = 0
+	movi	$3, 7					// $3 = 7
 
 p4while:
-	mov	$1, addr_j
-	ld	$1, $1
-	andi	$0, 7
-	cmp	$0, $1
-	beq	p2
-
-	mov	$2, addr_p
-	ld	$2, $2					// p
-	mov	$3, addr_t
-	ld	$3, $3					// t
-	xor	$2, $3
-	mov	$0, addr_p
-	st	$2, $0
-	shri	$3, 1
-	mov	$0, addr_t
-	st	$3, $0
-	incr	$1
-	mov	$0, addr_j
-	st	$1, $0
+	xor	$0, $1
+	shri	$1, 1
+	incr	$2
+	cmp	$3, $2
+	beqil p2
+	jmpil	p4while
 
 p2:
-	mov	$0, addr_p
-	ld	$0, $0
 	shli	$0, 3
-	andi	$1, 8
-	and	$0, $1
-	mov	$1, addr_p4
+	andi	$0, 8
+	movi	$1, addr_p4
 	st	$0, $1
 
-	mov	$0, addr_temp_upper
+	movi	$0, addr_temp_upper
 	ld	$0, $0
-	shri	$0, 1
-	mov	$1, addr_p
-	st	$0, $1
-	ld	$2, $1
-	shri	$2, 1
-	mov	$3, addr_t
-	st	$2, $3
-	xor	$0, $2
-	st	$0, $1
-	shri	$2, 2
-	st	$2, $3
-	xor	$0, $2
-	st	$0, $1
-	shri	$2, 1
-	st	$2, $3
-	xor	$0, $2
-	st	$0, $1
+	shri	$0, 1					// $0 = p
+	mov	$1, $0
+	shri	$1, 1					// $1 = t
+	xor	$0, $1
+	shri	$1, 2
+	xor	$0, $1
+	shri	$1, 1
+	xor	$0, $1
 
-	mov	$2, addr_lower
+	movi	$2, addr_lower
 	ld	$2, $2
-	mov	$3, addr_t
-	st	$2, $3
-	xor	$0, $2
-	st	$0, $1
-	shri	$2, 2
-	st	$2, $3
-	xor	$0, $2
-	st	$0, $1
-	shri	$2, 1
-	st	$2, $3
-	xor	$0, $2
-	st	$0, $1
+	mov	$1, $2
+	xor	$0, $1
+	shri	$1, 2
+	xor	$0, $1
+	shri	$1, 1
+	xor	$0, $1
 	shli	$0, 1
-	andi	$2, 2
-	and	$0, $2
-	mov	$1, addr_p2
+	andi	$0, 2
+	movi	$1, addr_p2
 	st	$0, $1
 
 p1:
-	mov	$0, addr_upper
-	ld	$0, $0
-	mov	$1, addr_p
-	st	$0, $1
-	shri	$0, 2
-	mov	$2, addr_t
-	st	$0, $2
-
-	andi $0, 0
-	mov	$1, addr_j
-	st	$0, $1					// j = 0
+	movi	$0, addr_temp_upper
+	ld	$0, $0					// $0 = p
+	mov	$1, $0
+	shri	$1, 2					// $1 = t
+	clr	$2
+	movi	$3, 3
 
 p1while:
-	mov	$1, addr_j
+	movi	$1, addr_j
 	ld	$1, $1
 	andi	$0, 3
 	cmp	$0, $1
 	beq	continue_while
 
-	mov	$2, addr_p
-	ld	$2, $2					// p
-	mov	$3, addr_t
-	ld	$3, $3					// t
-	xor	$2, $3
-	mov	$0, addr_p
-	st	$2, $0
-	shri	$3, 2
-	mov	$0, addr_t
-	st	$3, $0
-	incr	$1
-	mov	$0, addr_j
-	st	$1, $0
+	xor	$0, $1
+	shri	$1, 2
+	incr	$2
+	cmp	$3, $2
+	beqil continue_while
+	jmpil	p1while
 
 continue_while:
-	mov	$0, addr_lower
-	ld	$0, $0
-	mov	$1, addr_t
+	movi	$1, addr_lower
+	ld	$1, $1
+	xor	$0, $1
+	shri	$1, 1
+	xor	$0, $1
+	shri	$1, 2
+	xor	$0, $1
+	andi	$0, 1
+	movi	$1, addr_p1
 	st	$0, $1
-	mov	$2, addr_p
-	ld	$3, $2
-	xor	$3, $0
-	shri	$0, 1
-	xor	$3, $0
-	shri	$0, 2
-	xor	$3, $0
-	andi	$1, 1
-	and	$3, $1
-	mov	$0, addr_p1
-	st	$3, $0
 
-	mov	$0, addr_lower
+	movi	$0, addr_lower
 	ld	$0, $0
 	shli	$0, 3
-	andi	$1, 112
-	and	$0, $1
-	mov	$1, addr_b4to2
-	st	$0, $1
+	andi	$0, 112
 
-	mov	$2, addr_lower
+	movi	$2, addr_lower
 	ld	$2, $2
 	shli	$2, 2
-	andi	$1, 4
-	and	$2, $1
-	mov	$1, addr_b1
-	st	$2, $1
+	andi	$2, 4
 
 	or	$0, $2
-	mov	$1, addr_p8
+	movi	$1, addr_p8
 	ld	$1, $1
 	or	$0, $1
-	mov	$1, addr_p4
+	movi	$1, addr_p4
 	ld	$1, $1
 	or	$0, $1
-	mov	$1, addr_p2
+	movi	$1, addr_p2
 	ld	$1, $1
 	or	$0, $1
-	mov	$1, addr_p1
+	movi	$1, addr_p1
 	ld	$1, $1
 	or	$0, $1
 
-	mov	$1, addr_temp_lower
-	st	$0, $1
-
-	mov	$2, addr_k
+	movi	$2, addr_k
 	ld	$3, $2
 	st	$0, $3
-
 	incr	$3
-	mov	$1, addr_temp_upper
+	movi	$1, addr_temp_upper
 	ld	$1, $1
 	st	$1, $3
 	incr	$3
